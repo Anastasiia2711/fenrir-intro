d82285cdfcd106d02fb3ec9df7645a75dda74399
Anastasiia2711 <aeliseeva@Anasasiias-Air.lan>
2023-07-19 23:03:51 -0400

    Lesson 6-1

--- js/index.js
+++ js/index.js
@@ -6,23 +6,23 @@ copyright.innerHTML = `Anastasiia Eliseeva &copy ${thisYear}`;
 footer.appendChild(copyright);
 
 
-const skills = [id = 'JavaScript', 'Banking', 'Finance',  'GitHub', 'VSC', 'HTML', 'Cypress', 'Trello', 'Photoshop', 'Ligthroom', 'Bridge'];
+const skills = [id = 'JavaScript', 'Banking', 'Finance', 'GitHub', 'VSC', 'HTML', 'Cypress', 'Trello', 'Photoshop', 'Ligthroom', 'Bridge'];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 
- for (let i = 0; i < skills.length; i++) {
-     const skill = document.createElement('li');
-     skill.innerHTML = skills[i];
-     skillsList.appendChild(skill)
- }
+for (let i = 0; i < skills.length; i++) {
+    const skill = document.createElement('li');
+    skill.innerHTML = skills[i];
+    skillsList.appendChild(skill)
+}
 
- const messageSection = document.getElementById('messages');
- messageSection.hidden = true;
+const messageSection = document.getElementById('messages');
+messageSection.hidden = true;
 
 const messageForm = document.getElementsByName('leave_message');
 messageForm.item(0).addEventListener("submit", (event) => {
     event.preventDefault();
-   
+
     const name = event.target.usersName.value;
     const email = event.target.usersEmail.value;
     const message = event.target.usersMessage.value;
@@ -35,8 +35,8 @@ messageForm.item(0).addEventListener("submit", (event) => {
     const newMessage = document.createElement('li');
     const editMessage = document.createElement('li');
     newMessage.innerHTML = `<a href = "mailto: ${email}">${name}</a> send: <span>${message}</span>`;
-  
-    
+
+
     const removeButton = document.createElement('button');
     removeButton.innerText = 'remove';
     removeButton.type = 'button';
@@ -44,27 +44,52 @@ messageForm.item(0).addEventListener("submit", (event) => {
     removeButton.addEventListener('click', () => {
         const entry = removeButton.parentNode;
         entry.remove();
-   });
+    });
 
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
-//     const editButton = document.createElement('button');
-//     editButton.innerText = 'edit';
-//     editButton.type = 'button';
+    //     const editButton = document.createElement('button');
+    //     editButton.innerText = 'edit';
+    //     editButton.type = 'button';
 
-//     editButton.addEventListener('click', () => {
-//         const entry = editButton.parentNode;
-//         entry.edit();
-//    });
+    //     editButton.addEventListener('click', () => {
+    //         const entry = editButton.parentNode;
+    //         entry.edit();
+    //    });
 
-//     editMessage.appendChild(editButton);
-//     messageList.appendChild(editMessage);
-  
+    //     editMessage.appendChild(editButton);
+    //     messageList.appendChild(editMessage);
+
     messageForm.item(0).reset();
 
     messageSection.hidden = false;
 
-  });
-   
-  
+});
+
+//added GitHub links using AJAX
+
+const githubRequest = new XMLHttpRequest();
+githubRequest.open('GET', 'https://api.github.com/users/Anastasiia2711/repos');
+githubRequest.send();
+
+githubRequest.addEventListener('load', function () {
+    const repositories = JSON.parse(githubRequest.response);
+    console.log(repositories);
+
+    const projectSection = document.getElementById('projects');
+    // const projectSection = document.querySelector("#projects")
+    console.log(projectSection)
+    const projectList = projectSection.querySelector('ul');
+    console.log(projectList);
+
+    for (let i = 0; i < repositories.length; i++) {
+        const project = document.createElement('li');
+        project.innerHTML += `<a href="${repositories[i].html_url}" target="_blank">${repositories[i].name}</a>`
+        projectList.appendChild(project);
+    }
+});
+
+
+
+
