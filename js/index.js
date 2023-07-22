f6c937ea780a2a7a03e11291570331e1206da7d6
Anastasiia2711 <aeliseeva@Anasasiias-Air.lan>
2023-07-22 13:47:45 -0400

    Change my portfolio

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ copyright.innerHTML = `Anastasiia Eliseeva &copy ${thisYear}`;
 footer.appendChild(copyright);
 
 
-const skills = [id = 'JavaScript', 'Banking', 'Finance', 'GitHub', 'VSC', 'HTML', 'Cypress', 'Trello', 'Photoshop', 'Ligthroom', 'Bridge'];
+const skills = [id = 'JavaScript', 'Banking', 'Finance', 'GitHub', 'VSC', 'HTML', 'Cypress', 'Trello', 'Photoshop', 'Ligthroom', 'Bridge', 'CSS'];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 
